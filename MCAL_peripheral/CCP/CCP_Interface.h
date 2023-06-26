cec9c81b48327510109a91dc1c1f3f74f0a0944b
BassamMamdouhMohammed <bassammamdouhmohammed@gmail.com>
2023-06-27 00:22:48 +0300

    update CCP module to be generic for CCP1 & CCP2

--- MCAL_peripheral/CCP/CCP_Interface.h
+++ MCAL_peripheral/CCP/CCP_Interface.h
@@ -12,43 +12,74 @@
 #include "../GPIO/GPIO_Interface.h"
 #include "../Interrupt/Interrupt_Internal_Interface.h"
 /********************************* Macro Definitions ************************/
-#define _CCP_PWM_MODE_ENABLE     0X02
-#define _CCP_CAPTURE_MODE_ENABLE 0x01
-#define _CCP_COMPARE_MODE_ENABLE 0x00
-#define _CCP_MODE_SELECT _CCP_PWM_MODE_ENABLE
-#define _CCP_CAPTURE_TMR1      0
-#define _CCP_CAPTURE_TMR1_TMR3 1
-#define _CCP_CAPTURE_TMR3      2
+#define CCP_PWM_MODE_ENABLE     0X02
+#define CCP_CAPTURE_MODE_ENABLE 0x01
+#define CCP_COMPARE_MODE_ENABLE 0x00
+#define CCP_MODE_SELECT CCP_CAPTURE_MODE_ENABLE
 
-#define _CCP_MODULE_DISABLE                  ((uint8)0x00)
-#define _CCP_COMPARE_TOGGLE_OUTPUT           ((uint8)0x02)
-#define _CCP_CAPTURE_EVERY_FALLING_EDGE      ((uint8)0x04)
-#define _CCP_CAPTURE_EVERY_RISING_EDGE       ((uint8)0x05)
-#define _CCP_CAPTURE_EVERY_4th_RISING_EDGE   ((uint8)0x06)
-#define _CCP_CAPTURE_EVERY_16th_RISING_EDGE  ((uint8)0x07)
-#define _CCP_COMPARE_INITIALIZE_PIN_LOW      ((uint8)0x08)
-#define _CCP_COMPARE_INITIALIZE_PIN_HIGH     ((uint8)0x09)
-#define _CCP_COMPARE_GENERATE_SW_INTERRUPT   ((uint8)0x0A)
-#define _CCP_COMPARE_GENERATE_EVENT          ((uint8)0x0B)
-#define _CCP_PWM_MODE                        ((uint8)0x0C)
+#define INVALID_MODE                        ((uint8)0x03)
+#define INVALID_SUBMODE                     ((uint8)0x04)
+#define INVALID_MODULE                      ((uint8)0x05)
+#define CCP_CAPTURE_TMR1                    ((uint8)0x00)
+#define CCP_CAPTURE_TMR1_TMR3               ((uint8)0x01)
+#define CCP_CAPTURE_TMR3                    ((uint8)0x02)
 
-#define _CCP_CAPTURE_OCCURED     1
-#define _CCP_CAPTURE_NOT_OCCURED 0
+/* CCPx Module Mode Select bits */
+#define CCP_MODULE_DISABLE                  ((uint8)0x00)
+#define CCP_MODULE_ENABLE                   ((uint8)0x01) ///////
 
-#define _CCP_COMPARE_OCCURED     1
-#define _CCP_COMPARE_NOT_OCCURED 0
+#define CCP_CAPTURE_EVERY_FALLING_EDGE      ((uint8)0x04)
+#define CCP_CAPTURE_EVERY_RISING_EDGE       ((uint8)0x05)
+#define CCP_CAPTURE_EVERY_4th_RISING_EDGE   ((uint8)0x06)
+#define CCP_CAPTURE_EVERY_16th_RISING_EDGE  ((uint8)0x07)
 
-#define _CCP_TMR2_PRESCALER_DIVIDED_BY_1  1
-#define _CCP_TMR2_PRESCALER_DIVIDED_BY_4  4
-#define _CCP_TMR2_PRESCALER_DIVIDED_BY_16 16
+#define CCP_COMPARE_INITIALIZE_PIN_LOW      ((uint8)0x08)
+#define CCP_COMPARE_INITIALIZE_PIN_HIGH     ((uint8)0x09)
+#define CCP_COMPARE_GENERATE_SW_INTERRUPT   ((uint8)0x0A)
+#define CCP_COMPARE_GENERATE_SPECIAL_EVENT  ((uint8)0x0B)
+#define CCP_COMPARE_TOGGLE_OUTPUT           ((uint8)0x02)
+
+#define CCP_PWM_MODE                        ((uint8)0x0C)
+/* CCP Capture Mode status */
+#define CCP_CATURE_READY                    ((uint8)0x01)
+#define CCP_CATURE_NOT_READY                ((uint8)0x00)
+
+/* CCP Compare Mode status */
+#define CCP_COMPARE_READY                    ((uint8)0x01)
+#define CCP_COMPARE_NOT_READY                ((uint8)0x00)
+
+#define CCP_TIM2_POSTSCALER_DIV_BY_1       1
+#define CCP_TIM2_POSTSCALER_DIV_BY_2       2
+#define CCP_TIM2_POSTSCALER_DIV_BY_3       3
+#define CCP_TIM2_POSTSCALER_DIV_BY_4       4
+#define CCP_TIM2_POSTSCALER_DIV_BY_5       5
+#define CCP_TIM2_POSTSCALER_DIV_BY_6       6
+#define CCP_TIM2_POSTSCALER_DIV_BY_7       7
+#define CCP_TIM2_POSTSCALER_DIV_BY_8       8
+#define CCP_TIM2_POSTSCALER_DIV_BY_9       9
+#define CCP_TIM2_POSTSCALER_DIV_BY_10      10
+#define CCP_TIM2_POSTSCALER_DIV_BY_11      11
+#define CCP_TIM2_POSTSCALER_DIV_BY_12      12
+#define CCP_TIM2_POSTSCALER_DIV_BY_13      13
+#define CCP_TIM2_POSTSCALER_DIV_BY_14      14
+#define CCP_TIM2_POSTSCALER_DIV_BY_15      15
+#define CCP_TIM2_POSTSCALER_DIV_BY_16      16
+
+#define CCP_TIM2_PRESCALER_DIVIDED_BY_1     ((uint8)0x01)
+#define CCP_TIM2_PRESCALER_DIVIDED_BY_4     ((uint8)0x04)
+#define CCP_TIM2_PRESCALER_DIVIDED_BY_16    ((uint8)0x10)
+
+#define CCP_CAPTURE_TIM3                    ((uint8)0x00)
+#define CCP_CAPTURE_TIM1_TIM3               ((uint8)0x01)
+#define CCP_CAPTURE_TIM1                    ((uint8)0x02)
 /********************************* Macro Functions  *************************/
-#define CCP1_MODULE_SELECT_MODE(_Mode) (CCP1CONbits.CCP1M = _Mode)
-#define CCP2_MODULE_SELECT_MODE(_Mode) (CCP2CONbits.CCP2M = _Mode)
+
 /********************************* User-Defined Data Types ******************/
 typedef enum{
-    CCP1_inst =0,
-    CCP2_inst        
+    CCP1_instance,
+    CCP2_instance        
 }CCP_Module_t;
+
 typedef union{
     struct{
         uint8 ccpr_low;    /* CCPR1 low register  */
@@ -58,29 +89,51 @@ typedef union{
         uint16 ccpr_16Bit; /* Read CCPR1 as a 16 Bit value */
     };
 }CCPR_t;
-typedef struct{
-#if CCP_INTERRUPT_FEATURE_ENABLE==INTERRUPT_FEATURE_ENABLE  
-    void(* CCP_InterruptHandler)(void);
+
+typedef struct
+{
+#if CCP1_INTERRUPT_FEATURE_ENABLE==INTERRUPT_FEATURE_ENABLE  
+    void(* CCP1_InterruptHandler)(void);
 #if INTERRUPT_PRIORITY_ENABLE==INTERRUPT_FEATURE_ENABLE
-    Priority_t CCP_Priority;
+    Priority_t CCP1_Priority;
 #endif
 #endif
-    CCP_Module_t CCP_Select;
-    uint8 Mode :4;
-    uint32 Preload;
-    uint8 CCP_TMR_PSA;
+#if CCP2_INTERRUPT_FEATURE_ENABLE==INTERRUPT_FEATURE_ENABLE  
+    void(* CCP2_InterruptHandler)(void);
+#if INTERRUPT_PRIORITY_ENABLE==INTERRUPT_FEATURE_ENABLE
+    Priority_t CCP2_Priority;
+#endif
+#endif
+    
+    CCP_Module_t CCPx_Select;
+    uint8 Mode :2;
+    uint8 SubMode :4;
     PIN_t CCP_Pin;
-    uint8 Capture_TMR_Select;
-#if _CCP_MODE_SELECT==_CCP_PWM_MODE_ENABLE
+    uint8 Capture_TIM_Select;
+#if CCP_MODE_SELECT==CCP_PWM_MODE_ENABLE
     uint32 PWM_FREQ;
-    uint8 TMR2_Prescaler;
-    uint8 TMR2_Postscaler;
+    uint8 TMR2_Prescaler : 2;
+    uint8 TMR2_Postscaler : 4;
 #endif
 }CCP_t;
 /********************************* Functions Declaration ********************/
-STD_RETURN_t MCAL_CCP_Initialize(const CCP_t *_CCP);
-STD_RETURN_t MCAL_CCP_DeInitialize(const CCP_t *_CCP);
 
+STD_RETURN_t MCAL_CCP_Init(const CCP_t *ccp);
+STD_RETURN_t MCAL_CCP_DeInit(const CCP_t *ccp);
+
+#if (CCP_MODE_SELECT == CCP_CAPTURE_MODE_ENABLE)
+STD_RETURN_t MCAL_CCP_CAPUTER_DataReady(uint8 *capture_status);
+STD_RETURN_t MCAL_CCP_CAPUTER_ReadData(uint16 *data);
+
+#elif (CCP_MODE_SELECT == CCP_COMPARE_MODE_ENABLE)
+STD_RETURN_t MCAL_CCP_IsCompareComplete(uint8 *compare_status);
+STD_RETURN_t MCAL_CCP_CompareWriteValue(uint16 value);
+
+#elif (CCP_MODE_SELECT == CCP_PWM_MODE_ENABLE)
+STD_RETURN_t MCAL_CCP_PWM_START(const CCP_t *ccp);
+STD_RETURN_t MCAL_CCP_PWM_STOP(const CCP_t *ccp);
+STD_RETURN_t MCAL_CCP_PWM_SetDuty(const CCP_t *ccp, const uint8 duty);
+#endif
 
 #endif	/* CCP_INTERFACE_H */
 
--- MCAL_peripheral/CCP/CCP_Program.c
+++ MCAL_peripheral/CCP/CCP_Program.c
@@ -4,4 +4,486 @@
  * LinkedIn: https://www.linkedin.com/in/bassam-mamdouh
  * Created on September 28, 2022, 12:22 PM
  */
+#include <pic18f4620.h>
 #include "CCP_Interface.h"
+
+#if (CCP1_INTERRUPT_FEATURE_ENABLE==INTERRUPT_FEATURE_ENABLE)
+static void(*CCP1_InterruptHandler)(void) = NULL;
+#endif
+
+#if (CCP2_INTERRUPT_FEATURE_ENABLE==INTERRUPT_FEATURE_ENABLE)
+static void(*CCP2_InterruptHandler)(void) = NULL;
+#endif
+
+/*----------------------------- static helper function prototype ------------------------*/
+static STD_RETURN_t CCP_SelectCapureSubMode(const CCP_t *ccp);
+static STD_RETURN_t CCP_SelectCompareSubMode(const CCP_t *ccp);
+static STD_RETURN_t CCP_SelectPWMSubMode(const CCP_t *ccp);
+static void CCP_InterruptConfig(const CCP_t *ccp);
+static STD_RETURN_t CCP_Disable(const CCP_t *ccp);
+static STD_RETURN_t CCP_Enable(const CCP_t *ccp);
+static void CCP_CC_TimerSelect(const CCP_t *ccp);
+
+STD_RETURN_t MCAL_CCP_Init(const CCP_t *ccp)
+{
+    STD_RETURN_t retval = STD_OK;
+    
+    if(NULL != ccp)
+    {
+        retval = CCP_Disable(ccp);
+#if CCP_MODE_SELECT==CCP_CAPTURE_MODE_ENABLE
+        if(CCP_CAPTURE_MODE_ENABLE == ccp->Mode)
+        {
+            CCP_SelectCapureSubMode(ccp);
+        }
+#elif CCP_MODE_SELECT==CCP_COMPARE_MODE_ENABLE
+        if(CCP_COMPARE_MODE_ENABLE == ccp->Mode)
+        {
+            CCP_SelectCompareSubMode(ccp);
+        }
+#elif CCP_MODE_SELECT==CCP_PWM_MODE_ENABLE
+        if(CCP_PWM_MODE_ENABLE == ccp->Mode)
+        {
+            CCP_SelectPWMSubMode(ccp);
+            PR2 = (uint8)((_XTAL_FREQ / (ccp->PWM_FREQ * 4.0 
+                    * ccp->TMR2_Prescaler * ccp->TMR2_Postscaler)) - 1);
+        }
+#else
+        retval = INVALID_MODE;
+#endif
+
+        MCAL_GPIO_Pin_Initialize(&(ccp->CCP_Pin));
+        CCP_InterruptConfig(ccp);
+        CCP_Enable(ccp);
+    }
+    else
+    {
+        retval = STD_NOT_OK;
+    }
+    return retval;
+}
+STD_RETURN_t MCAL_CCP_DeInit(const CCP_t *ccp)
+{
+    STD_RETURN_t retval = STD_OK;
+    if(NULL != ccp){
+        if(CCP1_instance == ccp->CCPx_Select)
+        {
+            CCP1CONbits.CCP1M = CCP_MODULE_DISABLE;
+            #if CCP1_INTERRUPT_FEATURE_ENABLE==INTERRUPT_FEATURE_ENABLE
+                CCP1_INTERRUPT_DISABLE();
+            #endif   
+        }
+        else if(CCP2_instance == ccp->CCPx_Select)
+        {
+            CCP1CONbits.CCP1M = CCP_MODULE_DISABLE;
+            #if CCP2_INTERRUPT_FEATURE_ENABLE==INTERRUPT_FEATURE_ENABLE
+                CCP2_INTERRUPT_DISABLE();
+            #endif   
+        }
+        else
+        {
+           retval = INVALID_MODE;
+        }
+    }
+    else
+    {
+        retval = STD_NOT_OK;
+    }
+    return retval;
+}
+#if (CCP_MODE_SELECT == CCP_CAPTURE_MODE_ENABLE)
+STD_RETURN_t MCAL_CCP_CAPUTER_DataReady(uint8 *capture_status)
+{
+    STD_RETURN_t retval = STD_OK;
+    if(NULL != capture_status) 
+    {
+        if(CCP_CATURE_READY == PIR1bits.CCP1IF)
+        {
+            *capture_status = CCP_CATURE_READY;
+            CCP1_INTERRUPT_CLEAR_FLAG();
+        }
+        else
+        {
+            *capture_status = CCP_CATURE_NOT_READY;
+        }
+
+    }
+    else{retval = STD_NOT_OK;}
+    return retval;
+}
+STD_RETURN_t MCAL_CCP_CAPUTER_ReadData(uint16 *data)
+{
+    STD_RETURN_t retval = STD_OK;
+    CCPR_t temp = {.ccpr_low = 0, .ccpr_high = 0};
+    
+    if(NULL != data) 
+    {
+        temp.ccpr_low = CCPR1L;
+        temp.ccpr_high = CCPR1H;
+        *data = temp.ccpr_16Bit;
+    }
+    else{retval = STD_NOT_OK;}
+    return retval;
+}
+#endif
+
+#if (CCP_MODE_SELECT == CCP_COMPARE_MODE_ENABLE)
+STD_RETURN_t MCAL_CCP_IsCompareComplete(uint8 *compare_status)
+{
+    STD_RETURN_t retval = STD_OK;
+    if(NULL != compare_status) 
+    {
+        if(CCP_COMPARE_READY == PIR1bits.CCP1IF)
+        {
+            *compare_status = CCP_COMPARE_READY;
+            CCP1_INTERRUPT_CLEAR_FLAG();
+        }
+        else
+        {
+            *compare_status = CCP_COMPARE_NOT_READY;
+        }
+
+    }
+    else{retval = STD_NOT_OK;}
+    return retval;
+}
+STD_RETURN_t MCAL_CCP_CompareWriteValue(uint16 value)
+{
+    STD_RETURN_t retval = STD_OK;
+    CCPR_t temp = {.ccpr_low = 0, .ccpr_high = 0};
+    temp.ccpr_16Bit = value;
+    CCPR1L = temp.ccpr_low;
+    CCPR1H = temp.ccpr_high;
+    return retval;
+}
+#endif
+#if (CCP_MODE_SELECT == CCP_PWM_MODE_ENABLE)
+STD_RETURN_t MCAL_CCP_PWM_START(const CCP_t *ccp)
+{
+    STD_RETURN_t retval = STD_OK;
+    if(NULL != ccp) 
+    {
+        if(CCP1_instance == ccp->CCPx_Select)
+        {
+            CCP1CONbits.CCP1M = CCP_PWM_MODE;
+        }
+        else if(CCP2_instance == ccp->CCPx_Select)
+        {
+            CCP2CONbits.CCP2M = CCP_PWM_MODE;
+        }
+        else
+        {
+            retval = INVALID_MODE;
+        }
+    }
+    else
+    {
+        retval = STD_NOT_OK;
+    }
+    return retval;
+}
+STD_RETURN_t MCAL_CCP_PWM_STOP(const CCP_t *ccp)
+{
+    STD_RETURN_t retval = STD_OK;
+    if(NULL != ccp) 
+    {
+        if(CCP1_instance == ccp->CCPx_Select)
+        {
+            CCP1CONbits.CCP1M = CCP_MODULE_DISABLE;
+        }
+        else if(CCP2_instance == ccp->CCPx_Select)
+        {
+            CCP2CONbits.CCP2M = CCP_MODULE_DISABLE;
+        }
+        else
+        {
+            retval = INVALID_MODE;
+        }
+    }
+    else
+    {
+        retval = STD_NOT_OK;
+    }
+    return retval;
+}
+STD_RETURN_t MCAL_CCP_PWM_SetDuty(const CCP_t *ccp, const uint8 duty)
+{
+    STD_RETURN_t retval = STD_OK;
+    if(NULL != ccp) 
+    {
+        uint16 lduty = (uint16)(4 * (PR2 + 1)* (duty/100.0));
+        if(CCP1_instance == ccp->CCPx_Select)
+        {
+            CCP1CONbits.DC1B = (uint8)(lduty & 0x0003);
+            CCPR1L = (uint8)(lduty >> 2);
+        }
+        else if(CCP2_instance == ccp->CCPx_Select)
+        {
+            CCP2CONbits.DC2B = (uint8)(lduty & 0x0003);
+            CCPR2L = (uint8)(lduty >> 2);
+        }
+        else
+        {
+            retval = INVALID_MODE;
+        }
+    }
+    else
+    {
+        retval = STD_NOT_OK;
+    }
+    return retval;
+}
+#endif
+
+static STD_RETURN_t CCP_SelectCapureSubMode(const CCP_t *ccp)
+{
+    STD_RETURN_t retval = STD_OK;
+    if(CCP1_instance == ccp->CCPx_Select)
+    {
+        switch(ccp->SubMode)
+        {
+        case CCP_CAPTURE_EVERY_FALLING_EDGE:
+            CCP1CONbits.CCP1M = CCP_CAPTURE_EVERY_FALLING_EDGE;
+        break;
+        case CCP_CAPTURE_EVERY_RISING_EDGE:
+            CCP1CONbits.CCP1M = CCP_CAPTURE_EVERY_RISING_EDGE;
+        break;
+        case CCP_CAPTURE_EVERY_4th_RISING_EDGE:
+            CCP1CONbits.CCP1M = CCP_CAPTURE_EVERY_4th_RISING_EDGE;
+        break;
+        case CCP_CAPTURE_EVERY_16th_RISING_EDGE:
+            CCP1CONbits.CCP1M = CCP_CAPTURE_EVERY_16th_RISING_EDGE;
+        break;
+        default: retval = STD_NOT_OK;
+        }
+    }
+    else if(CCP2_instance == ccp->CCPx_Select)
+    {
+        switch(ccp->SubMode)
+        {
+        case CCP_CAPTURE_EVERY_FALLING_EDGE:
+            CCP2CONbits.CCP2M = CCP_CAPTURE_EVERY_FALLING_EDGE;
+        break;
+        case CCP_CAPTURE_EVERY_RISING_EDGE:
+            CCP2CONbits.CCP2M = CCP_CAPTURE_EVERY_RISING_EDGE;
+        break;
+        case CCP_CAPTURE_EVERY_4th_RISING_EDGE:
+            CCP2CONbits.CCP2M = CCP_CAPTURE_EVERY_4th_RISING_EDGE;
+        break;
+        case CCP_CAPTURE_EVERY_16th_RISING_EDGE:
+            CCP2CONbits.CCP2M = CCP_CAPTURE_EVERY_16th_RISING_EDGE;
+        break;
+        default: retval = STD_NOT_OK;
+        }
+    }
+    CCP_CC_TimerSelect(ccp);
+    return retval;
+}
+
+static STD_RETURN_t CCP_SelectCompareSubMode(const CCP_t *ccp)
+{
+    STD_RETURN_t retval = STD_OK;
+    if(CCP1_instance == ccp->CCPx_Select)
+    {
+        switch(ccp->SubMode)
+        {
+            case CCP_COMPARE_TOGGLE_OUTPUT:
+                CCP1CONbits.CCP1M = CCP_COMPARE_TOGGLE_OUTPUT;
+            break;
+            case CCP_COMPARE_INITIALIZE_PIN_LOW:
+                CCP1CONbits.CCP1M = CCP_COMPARE_INITIALIZE_PIN_LOW;
+            break;
+            case CCP_COMPARE_INITIALIZE_PIN_HIGH:
+                CCP1CONbits.CCP1M = CCP_COMPARE_INITIALIZE_PIN_HIGH;
+            break;
+            case CCP_COMPARE_GENERATE_SW_INTERRUPT:
+                CCP1CONbits.CCP1M = CCP_COMPARE_GENERATE_SW_INTERRUPT;
+            break;
+            case CCP_COMPARE_GENERATE_SPECIAL_EVENT:
+                CCP1CONbits.CCP1M = CCP_COMPARE_GENERATE_SPECIAL_EVENT;
+            break;
+            default: 
+                retval = INVALID_SUBMODE;
+        }
+    }
+    else if(CCP2_instance == ccp->CCPx_Select)
+    {
+        switch(ccp->SubMode)
+        {
+            case CCP_COMPARE_TOGGLE_OUTPUT:
+                CCP2CONbits.CCP2M = CCP_COMPARE_TOGGLE_OUTPUT;
+            break;
+            case CCP_COMPARE_INITIALIZE_PIN_LOW:
+                CCP2CONbits.CCP2M = CCP_COMPARE_INITIALIZE_PIN_LOW;
+            break;
+            case CCP_COMPARE_INITIALIZE_PIN_HIGH:
+                CCP2CONbits.CCP2M = CCP_COMPARE_INITIALIZE_PIN_HIGH;
+            break;
+            case CCP_COMPARE_GENERATE_SW_INTERRUPT:
+                CCP2CONbits.CCP2M = CCP_COMPARE_GENERATE_SW_INTERRUPT;
+            break;
+            case CCP_COMPARE_GENERATE_SPECIAL_EVENT:
+                CCP2CONbits.CCP2M = CCP_COMPARE_GENERATE_SPECIAL_EVENT;
+            break;
+            default: 
+                retval = INVALID_SUBMODE;
+        }
+    }
+    else
+    {
+        retval = INVALID_MODULE;
+    }
+    CCP_CC_TimerSelect(ccp);
+    return retval;
+}
+
+static STD_RETURN_t CCP_SelectPWMSubMode(const CCP_t *ccp)
+{
+    STD_RETURN_t retval = STD_OK;
+    if(CCP1_instance == ccp->CCPx_Select)
+    {
+        if(CCP_PWM_MODE == ccp->SubMode)
+        {
+            CCP1CONbits.CCP1M = CCP_PWM_MODE;
+        }
+        else
+        {
+            retval = INVALID_SUBMODE;
+        }
+       
+    }
+    else if(CCP2_instance == ccp->CCPx_Select)
+    {
+        if(CCP_PWM_MODE == ccp->SubMode)
+        {
+            CCP2CONbits.CCP2M = CCP_PWM_MODE;
+        }
+        else
+        {
+            retval = INVALID_SUBMODE;
+        }
+    }
+    else
+    {
+        retval = INVALID_MODULE;
+    }
+    return retval;
+}
+
+static void CCP_InterruptConfig(const CCP_t *ccp)
+{
+    
+#if (CCP1_INTERRUPT_FEATURE_ENABLE==INTERRUPT_FEATURE_ENABLE)
+      CCP1_INTERRUPT_DISABLE();
+      CCP1_INTERRUPT_CLEAR_FLAG();
+      CCP1_InterruptHandler = ccp->CCP1_InterruptHandler;
+#if (INTERRUPT_PRIORITY_ENABLE==INTERRUPT_FEATURE_ENABLE)
+      INTERRUPT_PRIORITY_LEVELS_ENABLE();
+      if(INTERRUPT_PRIORITY_HIGH == ccp->CCP1_Priority)
+      {
+          INTERRUPT_GLOBAL_HIGH_PRIORITY_ENABLE();
+          CCP1_INTERRUPT_HIGH_PRIORITY();
+      } 
+      else
+      {
+         INTERRUPT_GLOBAL_LOW_PRIORITY_ENABLE();
+         CCP1_INTERRUPT_LOW_PRIORITY(); 
+      } 
+#else
+      INTERRUPT_GLOBAL_ENABLE();
+      INTERRUPT_PERIPHERAL_ENABLE();
+#endif
+#endif
+      
+#if (CCP2_INTERRUPT_FEATURE_ENABLE==INTERRUPT_FEATURE_ENABLE)
+      CCP2_INTERRUPT_DISABLE();
+      CCP2_INTERRUPT_CLEAR_FLAG();
+      CCP2_InterruptHandler = ccp->CCP2_InterruptHandler;
+#if (INTERRUPT_PRIORITY_ENABLE==INTERRUPT_FEATURE_ENABLE)
+      INTERRUPT_PRIORITY_LEVELS_ENABLE();
+      if(INTERRUPT_PRIORITY_HIGH == ccp->CCP2_Priority)
+      {
+          INTERRUPT_GLOBAL_HIGH_PRIORITY_ENABLE();
+          CCP2_INTERRUPT_HIGH_PRIORITY();
+      } 
+      else
+      {
+         INTERRUPT_GLOBAL_LOW_PRIORITY_ENABLE();
+         CCP2_INTERRUPT_LOW_PRIORITY(); 
+      } 
+#else
+      INTERRUPT_GLOBAL_ENABLE();
+      INTERRUPT_PERIPHERAL_ENABLE();
+#endif
+#endif
+}
+
+static STD_RETURN_t CCP_Disable(const CCP_t *ccp)
+{
+    STD_RETURN_t retval = STD_OK;
+    if(CCP1_instance == ccp->CCPx_Select)
+    {
+        CCP1CONbits.CCP1M = CCP_MODULE_DISABLE;
+    }
+    else if(CCP2_instance == ccp->CCPx_Select)
+    {
+        CCP2CONbits.CCP2M = CCP_MODULE_DISABLE;
+    }
+    else
+    {
+        retval = INVALID_MODULE;
+    }
+    return retval;
+}
+
+static STD_RETURN_t CCP_Enable(const CCP_t *ccp)
+{
+    STD_RETURN_t retval = STD_OK;
+    if(CCP1_instance == ccp->CCPx_Select)
+    {
+        CCP1CONbits.CCP1M = CCP_MODULE_ENABLE;
+    }
+    else if(CCP2_instance == ccp->CCPx_Select)
+    {
+        CCP2CONbits.CCP2M = CCP_MODULE_ENABLE;
+    }
+    else
+    {
+        retval = INVALID_MODULE;
+    }
+    return retval;
+}
+static void CCP_CC_TimerSelect(const CCP_t *ccp)
+{
+    if(CCP_CAPTURE_TIM3 == ccp->Capture_TIM_Select)
+    {
+        T3CONbits.T3CCP1 = 0; 
+        T3CONbits.T3CCP2 = 1;
+    }
+    else if(CCP_CAPTURE_TIM1 == ccp->Capture_TIM_Select)
+    {
+        T3CONbits.T3CCP1 = 0;
+        T3CONbits.T3CCP2 = 0;
+    }
+    else if(CCP_CAPTURE_TIM1_TIM3 == ccp->Capture_TIM_Select)
+    {
+        T3CONbits.T3CCP1 = 1;
+        T3CONbits.T3CCP2 = 0;
+    }
+    else{}
+}
+void CCP1_ISR(void){
+    CCP1_INTERRUPT_CLEAR_FLAG();
+    if(CCP1_InterruptHandler){
+        CCP1_InterruptHandler();
+    }
+    else{ /* Nothing */ }
+}
+
+void CCP2_ISR(void){
+    CCP2_INTERRUPT_CLEAR_FLAG();
+    if(CCP2_InterruptHandler){
+        CCP2_InterruptHandler();
+    }
+    else{ /* Nothing */ }
+}
